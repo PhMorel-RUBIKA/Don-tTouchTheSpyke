3017c26aa466984cbbf8ecb11dd24567739ce488
yoann <y.pierkot@rubika-edu.com>
2022-02-10 12:56:54 +0100

    sprite manager base

---
+++ DTTS/Assets/Scripts/Systems/SpriteManager.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteManager : MonoBehaviour {
+    [SerializeField] private List<SpriteClass> spritelist = new List<SpriteClass>();
+
+}
+
+[System.Serializable]
+public class SpriteClass {
+    [SerializeField] private string name = "";
+    [SerializeField] private Sprite sprite = null;
+    [SerializeField] private int cost = 0;
+    public bool unlocked = false;
+
+    public string Name => name;
+    public Sprite Sprite => sprite;
+    public int Cost => cost;
+}
